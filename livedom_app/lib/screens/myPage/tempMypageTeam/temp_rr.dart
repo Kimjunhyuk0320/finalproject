df251ce23db294a8221161c70432e39f120479d8
Aster1 <gangjinsu4@gmail.com>
2024-01-16 18:35:17 +0900

    2401161835

---
+++ livedom_app/lib/screens/myPage/tempMypageTeam/temp_rr.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TempRRList extends StatefulWidget {
+  const TempRRList({super.key});
+
+  @override
+  State<TempRRList> createState() => _TempRRListState();
+}
+
+class _TempRRListState extends State<TempRRList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
